85f937021eb7e3ce211136bb77fa2f49b750a229
katongole <katsisaac50@gmail.com>
2018-09-14 18:17:26 +0200

    remove button added

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import '../style.css';
+import React from "react";
+
 function TodoItem(props) {
-    const todoTextClass = props.done
-        ? 'item-done'
-        : ''
-    // console.log(props)
-    return (
-        <li>
-            <input
-                type='checkbox'
-                onChange={() => {
-                props.changeStatus(props.id)
-            }}
-                checked={props.done}
-                />
-                <span className={todoTextClass}>{props.text}</span> 
-            <button  onClick={()=>{
-                props.removeTodo(props.id)
-            }}>remove</button>
-            
-        </li>
-    )
+  const todoTextClass = props.done ? "item-done" : "";
+  return (
+    <li>
+      <input
+        type="checkbox"
+        onChange={() => {
+          props.changeStatus(props.id);
+        }}
+        checked={props.done}
+      />
+      <span className={todoTextClass}>{props.text}</span>
+      <button  onClick={()=>{
+        props.removeTodo(props.id)
+    }}>remove</button>
+      </li>
+  );
 }
+
 export default TodoItem;
