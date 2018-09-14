d6769728e09ea5c59455c55de1cda2cf9cf2c483
katongole <katsisaac50@gmail.com>
2018-09-14 18:05:20 +0200

    some cleanup done

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 
-function TodoAdd(props){
-  //const inputValue=''
-
-    return (
-      
-      <div className= 'todo-add'>
-      <input 
-      type = 'text' 
-       onChange = {e => {
-           const input = e.target
-           const newValue =input.value
-        //console.log(e.target.value)
-        props.onChange(newValue);
-       }}
-       value={props.value} />
-      <button onClick={props.onButtonClick}> add todo</button>
+function TodoAdd(props) {
+  return (
+    <div className="todo-add">
+      <input
+        type="text"
+        onChange={e => {
+          const input = e.target;
+          const newValue = input.value;
+          props.onChange(newValue);
+        }}
+        value={props.value}
+      />
+      <button onClick={props.onButtonClick}>add Todo</button>
     </div>
-  )
+  );
 }
+
 export default TodoAdd;
