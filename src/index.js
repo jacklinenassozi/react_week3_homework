2e125d7af0b8e97d6838315d2ab8d5e6a0bb8d3f
katongole <katsisaac50@gmail.com>
2018-09-14 18:07:22 +0200

    midifications done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoApp from './components/TodoApp';
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoApp from "./components/TodoApp";
+import "./style.css";
 import Header from './Header';
-const root = document.getElementById('root')
 
-function App(){
-    return(
-        <div id='app'>
-        <Header />
-        <TodoApp />
-        </div>
-    )
+const root = document.getElementById("root");
+
+function App() {
+  return (
+    <div id="app">
+    <Header />
+    <TodoApp />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, root);
-//registerServiceWorker();
