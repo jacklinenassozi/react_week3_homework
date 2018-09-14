ad293ff5e843acebedff34f2b1a54ca73687d0ea
katongole <katsisaac50@gmail.com>
2018-09-14 18:18:46 +0200

    removeTodo props passed to todoItems

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import TodoItem from './TodoItem'
+import React from "react";
+import TodoItem from "./TodoItem";
 
-function TodoList(props){
-    console.log(props)
-return (
+function TodoList(props) {
+  return (
     <ul>
-    {props.items.length < 1 
-        ? 'no items'
-      :props.items.map((todo) => (
-       <TodoItem 
-       changeStatus={props.changeStatus} 
-       key = {todo.id}
-       text ={todo.description} 
-       done ={todo.done}
-       removeTodo={props.removeTodo}
-        />
-    ))}
+      {props.items.length < 1
+        ? "no items"
+        : props.items.map(todo => (
+            <TodoItem
+              changeStatus={props.changeStatus}
+              key={todo.id}
+              id={todo.id}
+              text={todo.description}
+              done={todo.done}
+            />
+          ))}
     </ul>
-)
+  );
 }
+
 export default TodoList;
